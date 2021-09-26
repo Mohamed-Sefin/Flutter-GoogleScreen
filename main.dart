bec03bc849022d527ebee1bf33be8fb9545fcdcc
Mohamed Sefin <72990155+Mohamed-Sefin@users.noreply.github.com>
2021-09-26 18:24:11 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import './layout.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Layout(),
+      ),
+    ),
+  );
+}
